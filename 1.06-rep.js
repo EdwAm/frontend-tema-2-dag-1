95e283e9f665544b39d283b59d6c89f320402ffd
Edward Amble <Edward@Edwards-MacBook-Air.local>
2019-09-20 09:21:24 +0200

    filnavnbytte

---
+++ 1.06-rep.js
@@ -0,0 +1,19 @@
+//ting som endrer seg er variabler, deffiner disse med let 
+let poeng = 0;
+
+//hvis jeg dreper en drage får jeg 10 poeng
+poeng += 10;
+
+//hvis jeg krasjer, så mister jeg 5 poeng 
+poeng -= 5; 
+
+//jeg kan miste alle poengene 
+poeng = 0; 
+
+//konstantene kan ikke endres 
+const PI = 3.14;
+const MAX_SPILLERE = 20; 
+
+console.log(PI);
+
+document.write(`Hei sann`)
